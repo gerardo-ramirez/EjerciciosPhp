b5995e55d8ace55e5ff4f9cb0a0f5cbc0badf611
gerardo <gerardoramirez656@gmail.com>
2019-03-20 16:16:57 -0300

    conexion a bd

--- login_registro/registrate.php
+++ login_registro/registrate.php
@@ -1,3 +1,46 @@
-<?php 
+<?php session_start(); //siempre desde el inicion y las paginas que trabajamos con sesiones. 
+
+//sesion con el valor de usuario iniciada.
+if(isset($_SESSION['usuario'])){ //['usuario'] es el name en el formulario.
+    
+    header('Location: index.php');
+
+}
+
+//recibir los datos del form_
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $usuario= filter_var(strtolower($_POST['usuario']), FILTER_SANITIZE_STRING);
+    $password= $_POST['password'] ;
+    $password2=$_POST['password2'];
+ 
+   
+    /*Aqui guardaremos los posibles errores que tengamos
+    y si no los hay entonces estara todo corecto.*/ 
+    $errores = "";
+    if( empty($usuario) or empty($password) or empty($password2)){
+        $errores .= "<li> Por favor rellena todos los datos correctamente </li>";
+    }else{
+
+        try {
+            $conexion = new PDO('mysql:host=127.0.0.1;dbname=registro_usuarios','root','');
+        
+        } catch (PDOExeption $e) {
+            echo "ERROR: " . $e->getMessage();
+            //si hay un error matamos la ejecucion de la pagina
+            //die();
+        
+        };
+        $statement= $conexion->prepare('SELECT * FROM registrados WHERE nombre = :usuario LIMIT 1');
+        $statement->execute(array(':usuario'=> $usuario));
+        $resultado = $statement->fetch();
+        if($resultado != false){
+            $errores .= "<li>El nombre ingresado ya existe.</li>";
+        }
+
+    }
+
+}
 require 'views/registrate.view.php';
+
+
 ?>
--- login_registro/views/login.view.php
+++ login_registro/views/login.view.php
@@ -8,11 +8,11 @@
 --><link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css?family=Raleway:300,400" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
-    <title>Registrate</title>
+    <title>Iniciar sesion</title>
 </head>
 <body>
 <div class="contenedor">
-<h1 class="titulo"> Registrate</h1>
+<h1 class="titulo"> Iniciar sesion</h1>
 
 <hr class="border"></hr>
 <!-- Usamos  la variable super global de $_SERVER para volver
@@ -21,29 +21,16 @@
 <div class="form-group">
 <i class="icono izquierda fa fa-user"></i><input type="text" name="usuario" class="usuario" placeholder="usuario">
 </div>
+
 <div class="form-group">
-<i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password" placeholder="password">
-</div>
-<div class="form-group">
-<i class="icono izquierda fa fa-user"></i><input type="password" name="password2" class="password_btn" placeholder="reapet"><i class="submit_btn fa fa-arrow-right" onclick="login.submit()"></i>
+<i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password_btn" placeholder="password"><i class="submit_btn fa fa-arrow-right" onclick="login.submit()"></i>
 <!-- el login es el que usamos en el name del formulario y la funcion submit es la propia del form
 lo que hacemos es activarla cuando se toque el icono  -->
 </div>
-<?php if(!empty($errores)) : ?>
-<div class="errores">
-<ul>
-<?php echo $errores?>
-
-</ul>
-</div>
-<?php endif ;?><!--Si usamos el if es necesario el endif-->
-
-
-
 </form>
 <p class="texto-registrate">
-¿ Ya tiene cuenta?
-<a href="login.php">Iniciar sesion</a>
+¿ Aún no tienes cuenta?
+<a href="registrate.php">Registrate</a>
 </p>
 
 
